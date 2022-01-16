9052b76869ea7950ab100b4265d53532b4a5371f
jhj0602 <jhj960918@gmail.com>
2022-01-16 15:43:26 +0900

    Feat : 프로젝트 초기 세팅

---
+++ src/main/java/com/maram/ssafe/SsafeApplication.java
@@ -0,0 +1,13 @@
+package com.maram.ssafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maram/ssafe/SsafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maram.ssafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
