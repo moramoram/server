499f2764231f448ce6eef69db0a50c9de06e6981
jhj0602 <jhj960918@gmail.com>
2022-01-19 01:58:49 +0900

    Refac : 코드 포멧 변경

--- src/main/java/com/moram/ssafe/SsafeApplication.java
+++ src/main/java/com/moram/ssafe/SsafeApplication.java
@@ -1,5 +1,6 @@
 package com.moram.ssafe;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class SsafeApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(SsafeApplication.class, args);
     }
 
 }
