53612f9f1025e1990368df99f2c3facb31bb5961
jhj0602 <jhj960918@gmail.com>
2022-01-19 01:19:22 +0900

    Feat : Google Oauth request

---
+++ src/main/java/com/moram/ssafe/dto/user/auth/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.moram.ssafe.dto.user.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    private String code;
+
+}
