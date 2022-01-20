ec3b40b446d566478b0fe32d0eb3a3bd9cc87445
jhj0602 <jhj960918@gmail.com>
2022-01-20 22:48:21 +0900

    Feat : Oauth 서버 통신 access token 처리 dto

---
+++ src/main/java/com/moram/ssafe/infrastructure/auth/OauthTokenResponse.java
@@ -0,0 +1,25 @@
+package com.moram.ssafe.infrastructure.auth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class OauthTokenResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    private String scope;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @Builder
+    public OauthTokenResponse(String accessToken, String scope, String tokenType) {
+        this.accessToken = accessToken;
+        this.scope = scope;
+        this.tokenType = tokenType;
+    }
+}
