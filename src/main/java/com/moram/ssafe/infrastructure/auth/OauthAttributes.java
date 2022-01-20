f692ca5f501be551c6f71970af6500fcef1f0dc6
jhj0602 <jhj960918@gmail.com>
2022-01-20 21:58:50 +0900

    Feat : Oauth Github, Google setting

---
+++ src/main/java/com/moram/ssafe/infrastructure/auth/OauthAttributes.java
@@ -0,0 +1,44 @@
+package com.moram.ssafe.infrastructure.auth;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public enum OauthAttributes {
+    GITHUB("github") {
+        @Override
+        public UserProfile of(Map<String, Object> attributes) {
+            return UserProfile.builder()
+                    .oauthId(String.valueOf(attributes.get("id")))
+                    .email((String) attributes.get("email"))
+                    .name((String) attributes.get("name"))
+                    .build();
+        }
+    },
+
+    GOOGLE("google") {
+        @Override
+        public UserProfile of(Map<String, Object> attributes) {
+            return UserProfile.builder()
+                    .oauthId(String.valueOf(attributes.get("sub")))
+                    .email((String) attributes.get("email"))
+                    .name((String) attributes.get("name"))
+                    .build();
+        }
+    };
+
+    private final String providerName;
+
+    OauthAttributes(String name) {
+        this.providerName = name;
+    }
+
+    public static UserProfile extract(String providerName, Map<String, Object> attributes) {
+        return Arrays.stream(values())
+                .filter(provider -> providerName.equals(provider.providerName))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new)
+                .of(attributes);
+    }
+
+    public abstract UserProfile of(Map<String, Object> attributes);
+}
