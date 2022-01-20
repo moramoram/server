0cbf44c05d56584fa931cde43831d33a7ab57a35
jhj0602 <jhj960918@gmail.com>
2022-01-20 21:46:38 +0900

    Feat : Oauth Api

--- src/main/java/com/moram/ssafe/controller/user/auth/AuthController.java
+++ src/main/java/com/moram/ssafe/controller/user/auth/AuthController.java
@@ -1,32 +1,24 @@
 package com.moram.ssafe.controller.user.auth;
 
-import com.moram.ssafe.dto.user.auth.LoginRequest;
+import com.moram.ssafe.dto.user.LoginResponse;
 import com.moram.ssafe.service.user.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/oauth")
+@RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/login/{socialType}")
-    public ResponseEntity<Void> login(@PathVariable String socialType, @RequestBody LoginRequest loginRequest,
-                                      HttpServletResponse response) {
-//        TokenResponse tokenResponse = authService.createAccessToken(socialType, loginRequest);
-//        Long id = authService.extractIdByToken(tokenResponse.getAccessToken(), JwtTokenType.ACCESS_TOKEN);
-//        ResponseCookie responseCookie = createRefreshTokenCookie(id);
-
-    log.info(loginRequest.getCode());
-        return new ResponseEntity<>(HttpStatus.CREATED);
-//        return ResponseEntity.ok(tokenResponse);
+    @GetMapping("/login/{socialType}")
+    public ResponseEntity<LoginResponse> login(@PathVariable String socialType, @RequestParam("code") String code) {
+        log.info(socialType+" "+code);
+        LoginResponse loginResponse = authService.login(socialType, code);
+        return ResponseEntity.ok().body(loginResponse);
     }
 }
