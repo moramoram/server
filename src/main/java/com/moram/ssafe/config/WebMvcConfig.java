f652743c9bf04c70d214e6f6fd50cafaa1d70c1b
jhj0602 <jhj960918@gmail.com>
2022-01-21 03:27:17 +0900

    Feat : 추후에 있을 CORS 설정 and Spring Interceptor url 설정

---
+++ src/main/java/com/moram/ssafe/config/WebMvcConfig.java
@@ -0,0 +1,32 @@
+package com.moram.ssafe.config;
+
+import com.moram.ssafe.infrastructure.auth.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/index.html");
+    }
+}
