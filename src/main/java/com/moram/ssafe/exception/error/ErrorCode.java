2a019aa7511a24e82608cecaf264443723ff1355
jhj0602 <jhj960918@gmail.com>
2022-01-21 03:24:05 +0900

    Feat : JWT 관련 에러코드 추가

--- src/main/java/com/moram/ssafe/exception/error/ErrorCode.java
+++ src/main/java/com/moram/ssafe/exception/error/ErrorCode.java
@@ -1,6 +1,7 @@
 package com.moram.ssafe.exception.error;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.moram.ssafe.exception.auth.UnsupportedJwtTokenException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,8 +11,18 @@ import lombok.Getter;
 public enum ErrorCode {
 
     INVALID_INPUT_VALUE(400, "Invalid input value."),
-    HELLO_ERROR_MESSAGE(404,"Hello 요청 실패"),
-    USER_NOT_FOUND(404, "User not found.");
+
+    //User
+    USER_NOT_FOUND(404, "User not found."),
+
+    //Auth and Jwt
+    FAIL_TO_AUTHENTICATION(401,"Fail To Authentication"),
+    NOT_EXIST_TOKEN(401,"Not exist token"),
+    EXPIRED_ACCESS_TOKEN(401, "Expired access token."),
+    INVALID_JWT_SIGNATURE(401,"Invalid JWT signature"),
+    UNSUPPORTED_JWT_TOKEN(401,"Unsupported Jwt Token"),
+    INVALID_JWT_TOKEN(401,"Invalid Jwt token");
+
 
     private final int status;
     private final String message;
