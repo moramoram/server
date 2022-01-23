42ecc3d564efc932286c4bdf59a260595dd696fd
jhj0602 <jhj960918@gmail.com>
2022-01-23 23:57:34 +0900

    Feat : Thread Local User

--- src/main/java/com/moram/ssafe/controller/user/annotation/UserContext.java
+++ src/main/java/com/moram/ssafe/controller/user/annotation/UserContext.java
@@ -1,7 +1,7 @@
-package com.moram.ssafe.controller.user.annotation;
-
-import com.moram.ssafe.domain.user.User;
-
-public class UserContext {
-    public static ThreadLocal<User> currentUser = new ThreadLocal<>();
-}
+//package com.moram.ssafe.controller.user.annotation;
+//
+//import com.moram.ssafe.domain.user.User;
+//
+//public class UserContext {
+//    public static ThreadLocal<User> currentUser = new ThreadLocal<>();
+//}
