50cb05b73c89f55d40318a7fcebe7ce899d672e9
jhj0602 <jhj960918@gmail.com>
2022-01-23 23:56:51 +0900

    Feat : Intetceptor url 설정

--- src/main/java/com/moram/ssafe/config/WebMvcConfig.java
+++ src/main/java/com/moram/ssafe/config/WebMvcConfig.java
@@ -27,6 +27,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/**")
+                .excludePathPatterns("/users/**")
                 .excludePathPatterns("/index.html");
     }
 }
