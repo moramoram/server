ae36215b4c657f2f87a71c55795cbfb76125b33c
jhj0602 <jhj960918@gmail.com>
2022-01-24 00:00:38 +0900

    Feat : user get, delete update

--- src/main/java/com/moram/ssafe/service/user/UserService.java
+++ src/main/java/com/moram/ssafe/service/user/UserService.java
@@ -1,7 +1,49 @@
 package com.moram.ssafe.service.user;
 
+import com.moram.ssafe.domain.user.User;
+import com.moram.ssafe.domain.user.UserRepository;
+import com.moram.ssafe.dto.user.UserProfileResponse;
+import com.moram.ssafe.dto.user.UserUpdateAddAuthRequest;
+import com.moram.ssafe.exception.user.UserNotFoundException;
+import com.mysql.cj.protocol.x.Notice;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sun.rmi.runtime.Log;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserProfileResponse getUserProfile(Long id) {
+        User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+        return UserProfileResponse.from(user);
+    }
+
+    @Transactional
+    public Long updateUserAddAuth(UserUpdateAddAuthRequest request) {
+        User originUser = userRepository.findById(request.getUserId()).orElseThrow(UserNotFoundException::new);
+        originUser.update(request);
+        return originUser.getId();
+    }
+
+    @Transactional
+    public void deleteUser(Long userId) {
+        User originUser = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        originUser.deleteUser();
+    }
+
+    public List<UserProfileResponse> userAuthApprove() {
+        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "modifiedDate"));
+        return users.stream().map(UserProfileResponse::from).collect(Collectors.toList());
+    }
 }
