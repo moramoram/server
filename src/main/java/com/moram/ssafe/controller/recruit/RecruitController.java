3bc88d419b0e7bff727b57001d00e036caa248c3
jhj0602 <jhj960918@gmail.com>
2022-01-25 02:57:59 +0900

    Feat : Recruit Domain Api

--- src/main/java/com/moram/ssafe/controller/recruit/RecruitController.java
+++ src/main/java/com/moram/ssafe/controller/recruit/RecruitController.java
@@ -1,4 +1,55 @@
 package com.moram.ssafe.controller.recruit;
 
+import com.moram.ssafe.dto.common.response.CommonResponseDto;
+import com.moram.ssafe.dto.common.response.SuccessMessage;
+import com.moram.ssafe.dto.recruit.RecruitSaveRequest;
+import com.moram.ssafe.dto.recruit.RecruitUpdateRequest;
+import com.moram.ssafe.service.recruit.RecruitService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequestMapping("/recruits")
+@RequiredArgsConstructor
 public class RecruitController {
+
+    private final RecruitService recruitService;
+
+    @GetMapping
+    public ResponseEntity<CommonResponseDto> findRecruitList() {
+        return ResponseEntity.ok().body(CommonResponseDto.of(
+                HttpStatus.OK, SuccessMessage.SUCCESS_GET_RECRUIT_LIST, recruitService.findRecruitList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CommonResponseDto> findRecruit(@PathVariable Long id) {
+        recruitService.addView(id);
+        return ResponseEntity.ok().body(CommonResponseDto.of(
+                HttpStatus.OK, SuccessMessage.SUCCESS_GET_RECRUIT, recruitService.findRecruit(id)));
+    }
+
+    @PostMapping
+    public ResponseEntity<CommonResponseDto> createRecruit(@RequestBody @Valid RecruitSaveRequest request) {
+        return ResponseEntity.ok().body(CommonResponseDto.of(
+                HttpStatus.OK, SuccessMessage.SUCCESS_POST_RECRUIT, recruitService.createRecruit(request)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CommonResponseDto> updateRecruit(@PathVariable Long id, @RequestBody @Valid RecruitUpdateRequest request) {
+        return ResponseEntity.ok().body(CommonResponseDto.of(
+                HttpStatus.OK, SuccessMessage.SUCCESS_PUT_RECRUIT, recruitService.updateRecruit(id, request)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<CommonResponseDto> deleteRecruit(@PathVariable Long id) {
+        recruitService.deleteRecruit(id);
+        return ResponseEntity.ok().body(CommonResponseDto.of(
+                HttpStatus.NO_CONTENT, SuccessMessage.SUCCESS_DELETE_RECRUIT, id));
+    }
 }
