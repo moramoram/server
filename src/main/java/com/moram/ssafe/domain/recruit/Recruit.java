55bc4f20dba8faa18f5e2584b83222614e89f269
jhj0602 <jhj960918@gmail.com>
2022-01-25 02:59:14 +0900

    Feat : Recruit enitity

--- src/main/java/com/moram/ssafe/domain/recruit/Recruit.java
+++ src/main/java/com/moram/ssafe/domain/recruit/Recruit.java
@@ -1,4 +1,85 @@
 package com.moram.ssafe.domain.recruit;
 
-public class Recruit {
+import com.moram.ssafe.domain.BaseEntity;
+import com.moram.ssafe.domain.company.Company;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "tbl_recruit")
+public class Recruit extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recruit_id", updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id", updatable = false)
+    private Company company;
+
+    private String title;
+
+    private String recruitUrl;
+
+    private String job;
+
+    private String empType;
+
+    private String career;
+
+    private String location;
+
+    private String techStack;
+
+    private LocalDateTime openDate;
+
+    private LocalDateTime closeDate;
+
+    private boolean sBenefit;
+
+    private String content;
+
+    private Integer views;
+
+    @Builder
+    public Recruit(Company company, String title, String recruitUrl, String job, String empType, String career, String location,
+                   String techStack, LocalDateTime openDate, LocalDateTime closeDate, boolean sBenefit, String content) {
+        this.company = company;
+        this.title = title;
+        this.recruitUrl = recruitUrl;
+        this.job = job;
+        this.empType = empType;
+        this.career = career;
+        this.location = location;
+        this.techStack = techStack;
+        this.openDate = openDate;
+        this.closeDate = closeDate;
+        this.sBenefit = sBenefit;
+        this.content = content;
+        this.views = 0;
+    }
+
+    public void addView() {
+        this.views++;
+    }
+
+    public void update(Recruit recruit) {
+        this.title = recruit.getTitle();
+        this.recruitUrl = recruit.getRecruitUrl();
+        this.job = recruit.getJob();
+        this.empType = recruit.getEmpType();
+        this.career = recruit.getCareer();
+        this.location = recruit.getLocation();
+        this.techStack = recruit.getTechStack();
+        this.openDate = recruit.getOpenDate();
+        this.closeDate = recruit.getCloseDate();
+        this.sBenefit = recruit.isSBenefit();
+        this.content = recruit.getContent();
+    }
+
 }
