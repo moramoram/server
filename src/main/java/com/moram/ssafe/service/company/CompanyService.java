494f9c93c8ca488e9b6817d1d0023749cdfba689
jhj0602 <jhj960918@gmail.com>
2022-01-27 01:47:52 +0900

    Feat : Company CRUD buiseness logic

---
+++ src/main/java/com/moram/ssafe/service/company/CompanyService.java
@@ -0,0 +1,78 @@
+package com.moram.ssafe.service.company;
+
+import com.moram.ssafe.config.s3.S3Uploader;
+import com.moram.ssafe.domain.company.Company;
+import com.moram.ssafe.domain.company.CompanyRepository;
+import com.moram.ssafe.dto.company.CompanyResponse;
+import com.moram.ssafe.exception.company.CompanyNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class CompanyService {
+
+    private final CompanyRepository companyRepository;
+    private final S3Uploader s3Uploader;
+
+    @Transactional
+    public Long createCompany(String companyName, String logoImg) {
+        Company company = companyRepository.save(
+                Company.builder()
+                        .companyName(companyName)
+                        .logoImg(logoImg)
+                        .build());
+        return company.getId();
+    }
+
+    public List<CompanyResponse> findCompanyList() {
+        return companyRepository.findAll(Sort.by(Sort.Direction.DESC, "id")).stream()
+                .map(CompanyResponse::from).collect(Collectors.toList());
+    }
+
+    public CompanyResponse findCompany(Long companyId) {
+        return CompanyResponse.from(getCompany(companyId));
+    }
+
+    public List<CompanyResponse> findCompanyName(String name) {
+        List<Company> companyList = companyRepository.findByCompanyNameContaining(name);
+        return companyList.stream()
+                .map(CompanyResponse::from).collect(Collectors.toList());
+
+    }
+
+    @Transactional
+    public void deleteCompany(Long companyId) {
+        Company company = getCompany(companyId);
+        s3Uploader.deleteObject(company.getLogoImg());
+        companyRepository.deleteById(company.getId());
+    }
+
+    @Transactional
+    public Long updateCompanyName(Long id, String name) {
+        Company originCompany = getCompany(id);
+        originCompany.updateCompanyName(name);
+        return originCompany.getId();
+    }
+
+    @Transactional
+    public Long updateLogoImg(Long id, String logoImgUrl) {
+        Company originCompany = getCompany(id);
+        originCompany.updateLogoImg(logoImgUrl);
+        return originCompany.getId();
+    }
+
+    public Company getCompany(Long id) {
+        return companyRepository.findById(id).orElseThrow(CompanyNotFoundException::new);
+    }
+
+
+}
