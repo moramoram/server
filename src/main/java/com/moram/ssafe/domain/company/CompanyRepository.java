b0ed64d356e3b4da63bc31eaf9d5c7db3be79d52
jhj0602 <jhj960918@gmail.com>
2022-01-27 01:42:11 +0900

    Feat : Company repository

--- src/main/java/com/moram/ssafe/domain/company/CompanyRepository.java
+++ src/main/java/com/moram/ssafe/domain/company/CompanyRepository.java
@@ -1,8 +1,14 @@
 package com.moram.ssafe.domain.company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company,Long> {
+
+    List<Company> findByCompanyNameContaining(@Param("name")String name);
 }
