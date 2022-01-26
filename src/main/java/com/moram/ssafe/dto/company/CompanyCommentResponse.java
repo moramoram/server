c0357661684ab9f9b3e6b8411a3fd751c5b47a5a
jhj0602 <jhj960918@gmail.com>
2022-01-27 04:18:38 +0900

    Feat : 회사 댓글 response

---
+++ src/main/java/com/moram/ssafe/dto/company/CompanyCommentResponse.java
@@ -0,0 +1,28 @@
+package com.moram.ssafe.dto.company;
+
+import com.moram.ssafe.domain.company.CompanyComment;
+import com.moram.ssafe.dto.user.UserResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class CompanyCommentResponse {
+
+    private Long commentId;
+
+    private UserResponse writer;
+
+    private String content;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime modifiedDate;
+
+    public static CompanyCommentResponse from(CompanyComment comment){
+        return new CompanyCommentResponse(comment.getId(),UserResponse.from(comment.getUser()),comment.getContent(),comment.getCreatedDate(),comment.getModifiedDate());
+    }
+
+}
