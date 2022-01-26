21e957cc7b1ad1774c83d871c43c04f7d0aba040
jhj0602 <jhj960918@gmail.com>
2022-01-27 04:18:20 +0900

    Feat : 회사 댓글 repository

---
+++ src/main/java/com/moram/ssafe/domain/company/CompanyCommentRepository.java
@@ -0,0 +1,12 @@
+package com.moram.ssafe.domain.company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CompanyCommentRepository extends JpaRepository<CompanyComment,Long> {
+
+    List<CompanyComment> findByCompanyId(Long companyId);
+}
