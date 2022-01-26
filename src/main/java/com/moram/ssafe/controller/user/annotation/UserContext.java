c6e4da27eaecdba186bcfd2b6027512ad52c1f8f
jhj0602 <jhj960918@gmail.com>
2022-01-26 23:50:24 +0900

    Feat : Thread Local User 구현

--- src/main/java/com/moram/ssafe/controller/user/annotation/UserContext.java
+++ src/main/java/com/moram/ssafe/controller/user/annotation/UserContext.java
@@ -1,7 +1,15 @@
 package com.moram.ssafe.controller.user.annotation;
 
-import com.moram.ssafe.domain.user.User;
+import com.moram.ssafe.exception.auth.InvalidJwtTokenException;
 
 public class UserContext {
-    public static ThreadLocal<User> currentUser = new ThreadLocal<>();
+
+    public static final ThreadLocal<JwtPayload> USER_CONTEXT = new ThreadLocal<>();
+
+    public static Long getCurrentUserId() {
+        if (UserContext.USER_CONTEXT.get() != null) {
+            return UserContext.USER_CONTEXT.get().getId();
+        }
+        throw new InvalidJwtTokenException();
+    }
 }
