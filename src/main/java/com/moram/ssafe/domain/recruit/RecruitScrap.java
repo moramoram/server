549de8da42770d2a67beb3d33f541f2bd5a6f227
jhj0602 <jhj960918@gmail.com>
2022-01-28 04:37:06 +0900

    Feat : Recruit scrap enitiy

--- src/main/java/com/moram/ssafe/domain/recruit/RecruitScrap.java
+++ src/main/java/com/moram/ssafe/domain/recruit/RecruitScrap.java
@@ -1,2 +1,44 @@
-package com.moram.ssafe.domain.recruit;public class RecruitScrap {
+package com.moram.ssafe.domain.recruit;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "tbl_recruit_scrap")
+public class RecruitScrap {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "scrap_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recruit_id", nullable = false)
+    private Recruit recruit;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Builder
+    public RecruitScrap(Long id, Recruit recruit, Long userId) {
+        this.id = id;
+        this.recruit = recruit;
+        this.userId = userId;
+    }
+
+    @Builder
+    public RecruitScrap( Recruit recruit, Long userId) {
+        this.recruit = recruit;
+        this.userId = userId;
+    }
+
+    public boolean ownedBy(Long userId) {
+        return this.userId.equals(userId);
+    }
 }
