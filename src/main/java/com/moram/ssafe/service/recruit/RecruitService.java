d25f3c5e94fdec2790cb1a8e1df55e77150b3424
jhj0602 <jhj960918@gmail.com>
2022-01-28 04:39:42 +0900

    Feat : 사용자 scrap 공고 리스트

--- src/main/java/com/moram/ssafe/service/recruit/RecruitService.java
+++ src/main/java/com/moram/ssafe/service/recruit/RecruitService.java
@@ -1,16 +1,21 @@
 package com.moram.ssafe.service.recruit;
 
+import com.moram.ssafe.controller.user.annotation.UserContext;
 import com.moram.ssafe.domain.company.Company;
 import com.moram.ssafe.domain.company.CompanyRepository;
 import com.moram.ssafe.domain.recruit.Recruit;
 import com.moram.ssafe.domain.recruit.RecruitRepository;
+import com.moram.ssafe.domain.recruit.RecruitScrap;
 import com.moram.ssafe.dto.recruit.RecruitResponse;
 import com.moram.ssafe.dto.recruit.RecruitSaveRequest;
+import com.moram.ssafe.dto.recruit.RecruitScrapResponse;
 import com.moram.ssafe.dto.recruit.RecruitUpdateRequest;
 import com.moram.ssafe.exception.company.CompanyNotFoundException;
 import com.moram.ssafe.exception.recruit.RecruitNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +31,7 @@ public class RecruitService {
     private final RecruitRepository recruitRepository;
     private final CompanyRepository companyRepository;
 
+
     public List<RecruitResponse> findRecruitList() {
         return recruitRepository.findAll().stream()
                 .map(RecruitResponse::from).collect(Collectors.toList());
@@ -38,7 +44,7 @@ public class RecruitService {
 
     @Transactional
     public Long createRecruit(RecruitSaveRequest request) {
-        Company company = companyRepository.findById(request.getCompanyId()).orElseThrow(CompanyNotFoundException::new);
+        Company company = getCompany(request.getCompanyId());
         Recruit recruit = recruitRepository.save(request.toRecruit(company));
         return recruit.getId();
     }
@@ -63,6 +69,28 @@ public class RecruitService {
         recruit.addView();
     }
 
+    public List<RecruitResponse> findUserRecruitScrapList(int limit) {
+        Long userId = UserContext.getCurrentUserId();
+        PageRequest pageRequest = PageRequest.of(limit-1, 2, Sort.by("id").descending());
+        List<Recruit> recruits = recruitRepository.findByUserScrap(userId, pageRequest);
+        return recruits.stream()
+                .map(RecruitResponse::from).collect(Collectors.toList());
+
+    }
+
+    @Transactional
+    public RecruitScrapResponse toggleRecruitScraps(Long recruitId) {
+        Long userId = UserContext.getCurrentUserId();
+        Recruit recruit = getRecruit(recruitId);
+        RecruitScrap recruitScrap = RecruitScrap.builder()
+                .recruit(recruit)
+                .userId(userId)
+                .build();
+        return RecruitScrapResponse.from(
+                recruit.toggleRecruitScarp(recruitScrap)
+        );
+    }
+
     public Recruit getRecruit(Long id) {
         return recruitRepository.findById(id).orElseThrow(RecruitNotFoundException::new);
     }
@@ -70,4 +98,5 @@ public class RecruitService {
     public Company getCompany(Long id) {
         return companyRepository.findById(id).orElseThrow(CompanyNotFoundException::new);
     }
+
 }
