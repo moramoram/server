63a47cdd02ec1f21776e1d9ce365e8da12c53965
jhj0602 <jhj960918@gmail.com>
2022-01-28 04:38:42 +0900

    Feat : Scrap status

--- src/main/java/com/moram/ssafe/dto/recruit/RecruitScrapResponse.java
+++ src/main/java/com/moram/ssafe/dto/recruit/RecruitScrapResponse.java
@@ -1,2 +1,16 @@
-package com.moram.ssafe.dto.recruit;public class RecruitScrapResponse {
+package com.moram.ssafe.dto.recruit;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecruitScrapResponse {
+    private boolean scrap;
+
+    public static RecruitScrapResponse from(boolean scrap){
+        return new RecruitScrapResponse(scrap);
+    }
 }
