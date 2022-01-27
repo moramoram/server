89e43851724899b32f25feae1c82ffeafaea39db
jhj0602 <jhj960918@gmail.com>
2022-01-28 04:39:19 +0900

    Feat : 스크랩 Exception

--- src/main/java/com/moram/ssafe/exception/recruit/RecruitScrapRemovalFailureException.java
+++ src/main/java/com/moram/ssafe/exception/recruit/RecruitScrapRemovalFailureException.java
@@ -1,2 +1,11 @@
-package com.moram.ssafe.exception.recruit;public class RecruitScrapRemovalFailureException {
+package com.moram.ssafe.exception.recruit;
+
+import com.moram.ssafe.exception.error.ErrorCode;
+import com.moram.ssafe.exception.error.SafeServerException;
+
+public class RecruitScrapRemovalFailureException extends SafeServerException {
+    public RecruitScrapRemovalFailureException() {
+        super(ErrorCode.RECRUIT_SCRAP_REMOVE_FAIL);
+    }
+
 }
