aa44b8a33b9f1640a6774ab8d45c0c70f225d5ea
jhj0602 <jhj960918@gmail.com>
2022-01-28 04:36:42 +0900

    Fix : 페이징 N+1 문제 jpql 해결

--- src/main/java/com/moram/ssafe/domain/recruit/RecruitRepository.java
+++ src/main/java/com/moram/ssafe/domain/recruit/RecruitRepository.java
@@ -1,9 +1,12 @@
 package com.moram.ssafe.domain.recruit;
 
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -19,5 +22,9 @@ public interface RecruitRepository extends JpaRepository<Recruit,Long> {
     @EntityGraph(attributePaths = {"company"})
     List<Recruit> findAll();
 
+    @EntityGraph(attributePaths = {"company"})
+    @Query(value = "select r from Recruit r join RecruitScrap rc " +
+            "on r.id = rc.recruit.id and rc.userId = :userId")
+    List<Recruit> findByUserScrap(@Param("userId") Long userId,  Pageable pageable);
 
 }
