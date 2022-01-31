8ed504df86ce1b1bfbb2dbe127d772a7664342fd
jhj0602 <jhj960918@gmail.com>
2022-01-31 20:11:09 +0900

    Feat : Role auth 추가

--- src/main/java/com/moram/ssafe/domain/user/Role.java
+++ src/main/java/com/moram/ssafe/domain/user/Role.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum Role {
 
-    ADMIN("ROLE_ADMIN"), USER("ROLE_USER");
+    ADMIN("ROLE_ADMIN"),AUTH("ROLE_AUTH"), USER("ROLE_USER");
 
     private String role;
 
