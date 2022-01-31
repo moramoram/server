20cbdf632332f65f6f689b81ab9b7865123b5cce
jhj0602 <jhj960918@gmail.com>
2022-01-31 20:13:54 +0900

    Feat : 관리자 사용자 권한 부여

---
+++ src/main/java/com/moram/ssafe/dto/user/UserAuthResponse.java
@@ -0,0 +1,30 @@
+package com.moram.ssafe.dto.user;
+
+import com.moram.ssafe.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class UserAuthResponse {
+
+    private Long id;
+    private String email;
+    private String profileImg;
+    private String nickname;
+    private String realName;
+    private int ordinal;
+    private String campus;
+    private int authCheck;
+    private String likeJob;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    public static UserAuthResponse from(User user) {
+        return new UserAuthResponse(user.getId(), user.getEmail(),
+                user.getProfileImg(), user.getNickname(), user.getRealName(), user.getOrdinal(),
+                user.getCampus(), user.getAuthCheck(), user.getLikeJob(), user.getCreatedDate(), user.getModifiedDate());
+    }
+}
