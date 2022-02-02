5c69e22b6b85d65d18b2d3e2fe999f7c395ec051
jhj0602 <jhj960918@gmail.com>
2022-02-02 23:53:21 +0900

    Refac : 단건 조회 및 리스트 조회 dto 변경

--- src/main/java/com/moram/ssafe/dto/recruit/RecruitResponse.java
+++ src/main/java/com/moram/ssafe/dto/recruit/RecruitResponse.java
@@ -1,14 +1,20 @@
 package com.moram.ssafe.dto.recruit;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.moram.ssafe.domain.recruit.Recruit;
 import com.moram.ssafe.dto.company.CompanyResponse;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor
+@Builder
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RecruitResponse {
 
     private Long recruitId;
@@ -39,18 +45,33 @@ public class RecruitResponse {
 
     private Integer views;
 
-    private boolean likeStatus;
+    private boolean scrapStatus;
 
     private LocalDateTime createdDate;
 
     private LocalDateTime modifiedDate;
 
+
     public static RecruitResponse from(Recruit recruit) {
+        return RecruitResponse.builder()
+                .recruitId(recruit.getId())
+                .company(CompanyResponse.from(recruit.getCompany()))
+                .title(recruit.getTitle())
+                .job(recruit.getJob())
+                .openDate(recruit.getOpenDate())
+                .closeDate(recruit.getCloseDate())
+                .sBenefit(recruit.isSBenefit())
+                .techStack(recruit.getTechStack())
+                .scrapStatus(false)
+                .build();
+    }
+
+    public static RecruitResponse from(Recruit recruit, boolean scrapStatus) {
         return new RecruitResponse(
                 recruit.getId(), CompanyResponse.from(recruit.getCompany()), recruit.getTitle(),
                 recruit.getRecruitUrl(), recruit.getJob(), recruit.getEmpType(), recruit.getCareer(),
                 recruit.getLocation(), recruit.getTechStack(), recruit.getOpenDate(), recruit.getCloseDate(),
-                recruit.isSBenefit(), recruit.getContent(), recruit.getViews(), false,
+                recruit.isSBenefit(), recruit.getContent(), recruit.getViews(), scrapStatus,
                 recruit.getCreatedDate(), recruit.getModifiedDate()
         );
     }
