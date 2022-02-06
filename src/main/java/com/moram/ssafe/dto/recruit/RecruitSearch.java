ba6f605fc3019a2057ea7090be7baf5162e1a6c5
jhj0602 <jhj960918@gmail.com>
2022-02-06 21:21:12 +0900

    Feat : 정렬 조건 추가

--- src/main/java/com/moram/ssafe/dto/recruit/RecruitSearch.java
+++ src/main/java/com/moram/ssafe/dto/recruit/RecruitSearch.java
@@ -3,15 +3,23 @@ package com.moram.ssafe.dto.recruit;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class RecruitSearch {
 
+    @NotNull(message = "null을 허용하지 않습니다.")
     private String title;
 
+    @NotNull(message = "null을 허용하지 않습니다.")
     private List<String> techStack;
 
+    @NotNull(message = "null을 허용하지 않습니다.")
     private String job;
+
+    @NotNull(message = "null을 허용하지 않습니다.")
+    private String criteria;
 }
