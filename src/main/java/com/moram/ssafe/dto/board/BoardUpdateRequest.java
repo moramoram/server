1c62c50b7abe5674f5165bab53d248da965fe798
wangsuuu <wsr5683@naver.com>
2022-02-07 15:38:57 +0900

    Refac : Board Request DTO validation 추가

--- src/main/java/com/moram/ssafe/dto/board/BoardUpdateRequest.java
+++ src/main/java/com/moram/ssafe/dto/board/BoardUpdateRequest.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @AllArgsConstructor
 public class BoardUpdateRequest {
 
     @NotBlank(message = "제목이 없습니다.")
+    @Size(max = 45, message = "1자 이상 45자 이하여야 합니다.")
     private String title;
 
     @NotBlank(message = "내용이 없습니다.")
