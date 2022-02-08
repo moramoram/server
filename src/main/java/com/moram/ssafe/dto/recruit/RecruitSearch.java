2a16796aafed74725a707f214679901b832929fa
jhj0602 <jhj960918@gmail.com>
2022-02-08 20:14:06 +0900

    Style : 코드 포멧 변경

--- src/main/java/com/moram/ssafe/dto/recruit/RecruitSearch.java
+++ src/main/java/com/moram/ssafe/dto/recruit/RecruitSearch.java
@@ -1,9 +1,7 @@
 package com.moram.ssafe.dto.recruit;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
